ee89dac4303e5221d303be6c3d1e9dd8d20ce8a1
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-06-12 02:02:58 +0700

    Favorite Unit Testing fix. Instrumental test favorite

--- app/src/androidTest/java/com/example/cinema/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/cinema/MainActivityTest.kt
@@ -2,19 +2,25 @@ package com.example.cinema
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.cinema.data.MovieRepository
 import com.example.cinema.data.response.ResultMovies
 import com.example.cinema.data.response.ResultTVShow
+import com.example.cinema.room.entity.MovieEntity
+import com.example.cinema.ui.favorite.movies.MoviesViewModel
 import com.example.cinema.ui.home.HomeViewModel
 import com.example.cinema.utils.EspressoIdlingResource
 import com.example.cinema.utils.Resource
@@ -26,7 +32,9 @@ import org.junit.Test
 class MainActivityTest {
     private lateinit var dummyMovies : LiveData<Resource<List<ResultMovies>>>
     private lateinit var dummyTVShow : LiveData<Resource<List<ResultTVShow>>>
+    private lateinit var dummyFavoriteMovies : DataSource.Factory<Int, MovieEntity>
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var favoriteMoviesViewModel: MoviesViewModel
     private lateinit var movieRepository: MovieRepository
     private lateinit var instrumentationContext: Context
 
@@ -35,6 +43,7 @@ class MainActivityTest {
         instrumentationContext = InstrumentationRegistry.getInstrumentation().context
         movieRepository = MovieRepository(instrumentationContext)
         homeViewModel = HomeViewModel(movieRepository)
+        favoriteMoviesViewModel = MoviesViewModel(movieRepository)
         IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
     }
 
@@ -63,5 +72,55 @@ class MainActivityTest {
         onView(withId(R.id.rv_popular_tv)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
             dummyTVShow.value?.data?.size!!
         ))
+    }
+
+    @Test
+    fun loadFavorite() {
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+        onView(withContentDescription(R.string.title_favorite)).perform(ViewActions.click())
+        onView(withId(R.id.tabs)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager)).check(matches(isDisplayed()))
+        onView(withContentDescription(R.string.movies)).perform(ViewActions.click())
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun insertFavorite(){
+        dummyMovies = homeViewModel.getMoviesPopular()
+        onView(withId(R.id.rv_popular)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_popular)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.value?.data?.size!!))
+        onView(withId(R.id.rv_popular)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        onView(withId(R.id.tv_title_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title_movies)).check(matches(withText(dummyMovies.value?.data!![0].title)))
+        onView(withId(R.id.fab_favorite_movies)).perform(ViewActions.click())
+        onView(withId(R.id.fab_favorite_movies)).perform(ViewActions.pressBack())
+
+        onView(withContentDescription(R.string.title_favorite)).perform(ViewActions.click())
+        onView(withId(R.id.tabs)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager)).check(matches(isDisplayed()))
+        onView(withContentDescription(R.string.movies)).perform(ViewActions.click())
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        onView(withId(R.id.tv_title_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title_movies)).check(matches(withText(dummyMovies.value?.data!![0].title)))
+    }
+
+    @Test
+    fun deleteFavorite(){
+        dummyMovies = homeViewModel.getMoviesPopular()
+        onView(withId(R.id.rv_popular)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_popular)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.value?.data?.size!!))
+        onView(withId(R.id.rv_popular)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        onView(withId(R.id.tv_title_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title_movies)).check(matches(withText(dummyMovies.value?.data!![0].title)))
+        onView(withId(R.id.fab_favorite_movies)).perform(ViewActions.click())
+        onView(withId(R.id.fab_favorite_movies)).perform(ViewActions.pressBack())
+
+        onView(withContentDescription(R.string.title_favorite)).perform(ViewActions.click())
+        onView(withId(R.id.tabs)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager)).check(matches(isDisplayed()))
+        onView(withContentDescription(R.string.movies)).perform(ViewActions.click())
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movies)).check(matches(hasChildCount(0)))
     }
 }
--- app/src/test/java/com/example/cinema/ui/utils/PagedListUtil.kt
+++ app/src/test/java/com/example/cinema/ui/utils/PagedListUtil.kt
@@ -7,11 +7,11 @@ import org.mockito.Mockito
 object PagedListUtil {
     fun <T> mockPagedList(list: List<T>): PagedList<T> {
         val pagedList = Mockito.mock(PagedList::class.java) as PagedList<T>
-        Mockito.`when`(pagedList[ArgumentMatchers.anyInt()]).then { invocation ->
+        Mockito.lenient().`when`(pagedList[ArgumentMatchers.anyInt()]).then { invocation ->
             val index = invocation.arguments.first() as Int
             list[index]
         }
-        Mockito.`when`(pagedList.size).thenReturn(list.size)
+        Mockito.lenient().`when`(pagedList.size).thenReturn(list.size)
 
         return pagedList
     }
