c4cb07432eb1d03f8e50beeeb4bd657fe56ca31d
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-06-14 06:24:53 +0700

    Last commit

--- app/src/test/java/com/example/cinema/data/MovieRepositoryTest.kt
+++ app/src/test/java/com/example/cinema/data/MovieRepositoryTest.kt
@@ -1,38 +1,88 @@
 package com.example.cinema.data
 
-import junit.framework.TestCase
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.DataSource
+import com.example.cinema.room.MovieDao
+import com.example.cinema.room.entity.MovieEntity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-class MovieRepositoryTest : TestCase() {
+@RunWith(MockitoJUnitRunner::class)
+class MovieRepositoryTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    fun testGetAllMoviesDB() {}
+    private lateinit var movieRepository: MovieRepository
 
+    @Mock
+    private lateinit var movieDao: MovieDao
+
+    @Mock
+    private lateinit var mockContext: Context
+
+    @Before
+    fun setUp() {
+        movieRepository = MovieRepository(mockContext)
+    }
+
+    @Test
+    fun testGetAllMoviesDB() {
+        val dataSourceFactory = Mockito.mock(DataSource.Factory::class.java) as DataSource.Factory<Int, MovieEntity>
+        Mockito.`when`(movieRepository.getAllMoviesDB()).thenReturn(dataSourceFactory)
+        Mockito.`when`(movieDao.getMovies()).thenReturn(null)
+
+        val resultViewModel = movieRepository.getAllMoviesDB()
+        Mockito.verify(movieDao).getMovies()
+    }
+
+    @Test
     fun testInsertMoviesDB() {}
 
+    @Test
     fun testGetMoviesByIdDB() {}
 
+    @Test
     fun testGetMoviesByIdDBLiveData() {}
 
+    @Test
     fun testDeleteMoviesDB() {}
 
+    @Test
     fun testGetAllTVShowDB() {}
 
+    @Test
     fun testInsertTVShowDB() {}
 
+    @Test
     fun testGetTVShowByIdDB() {}
 
+    @Test
     fun testGetTVShowByIdDBLiveData() {}
 
+    @Test
     fun testDeleteTVShowDB() {}
 
+    @Test
     fun testGetMoviesPopular() {}
 
+    @Test
     fun testGetTVShowPopular() {}
 
+    @Test
     fun testGetMoviesSearch() {}
 
+    @Test
     fun testGetShowSearch() {}
 
+    @Test
     fun testGetMovieDetail() {}
 
+    @Test
     fun testGetTVShowDetail() {}
 }
